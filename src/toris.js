ce9ad80de79778a17a9134eb571898a42467e276
GersonBelJr <gersonbeljr@gmail.com>
2024-03-22 17:07:58 -0300

    Altos bagulho

--- src/toris.js
+++ src/toris.js
@@ -1,12 +1,21 @@
-function extraiLinks(arraylinks) {
-    return arraylinks.map((objLink)=>Object.values(objLink).join(","))
+async function checaStatus(listaURL) {
+    const arrStatus = await Promise.all(
+        listaURL.map(async (url) => {
+            const response = await fetch(url);
+            return response.status;
+        })
+    )
+    return arrStatus;
 }
 
-
-
-
+function extraiLinks(arraylinks) {
+    return arraylinks.map((objLink) => Object.values(objLink).join(","))
+}
 
 //
-export default function listaValidada(listaDelinks){
-    return extraiLinks(listaDelinks);
+export default async function listaValidada(listaDelinks) {
+    //return extraiLinks(listaDelinks);
+    const links = extraiLinks(listaDelinks);
+    const status = await checaStatus(links);
+    return status
 }
